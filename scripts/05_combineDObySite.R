a0e301c376a1b2b9aa199a8c428bb56e49e51b04
Jessie Ribera <jessierosaribera@gmail.com>
2026-03-11 17:00:44 -0600

    started looking at distubance data here

--- scripts/05_combineDObySite.R
+++ scripts/05_combineDObySite.R
@@ -8,39 +8,34 @@
 # The section below that "cleans up" the environment removes raw EXO files before pushing changes. 
 
 #### libraries ####
+library(googledrive) #so we can interface with Google files
 library(tidyverse) #so we can a small amount of data transformation
 library(lubridate) #so we can change transform date-time data
 
-#### Add data to R and do a minor clean ####
-#upload all data from local 03_raw_MX801 folder
-local <-"~/Library/CloudStorage/OneDrive-UniversityofNewMexico/UNM/BEGI/Data/03_raw_MX801"
-do_list<- list.files(path = local, full.names = TRUE, pattern = ".csv") #turns into a list
-do_data <- do_list %>% 
-  set_names(~sub("\\.csv$", "", basename(.))) %>% #sets name to original files
-  map(read_csv) #reads in files to our environment
+#### Add data to R ####
+#add DO data
+do_data <- read_csv("~/Library/CloudStorage/OneDrive-UniversityofNewMexico/UNM/BEGI/Data/05_combined_cleaned/do_raw_combined.csv")
 
-#creates a function that I can run on all my files listed
-clean_do<- function(df) { 
-  df |>
-    rename(date = 2, temp_C = 3, do_mg_L = 4) |> #renames cols to simpler names
-    mutate(date = mdy_hms(date))|>
-    mutate(date = round_date(date, unit = "5 minute")) |> # transforms all times to the nearest 5 mins
-    filter(minute(date) %in% c(0, 15, 30, 45)) #removes any data that is not on the 0, 15, 30, or 45 minute mark (some of the earlier ones were set to log every 5 mins)
-}
+### Download disturbance data ###
+dist <- drive_get ("https://docs.google.com/spreadsheets/d/1T3NfyaLZTzo4YIrDL2GRKJr_MzR5IzlW/edit?gid=1054504098#gid=1054504098")
+drive_download(dist, path = "~/Library/CloudStorage/OneDrive-UniversityofNewMexico/UNM/BEGI/Data/w_visits.xlsx", overwrite = TRUE)
+w_visits <- read_csv ("~/Library/CloudStorage/OneDrive-UniversityofNewMexico/UNM/BEGI/Data/w_visits.csv")
 
-do_data <- map(do_data, clean_do) #applies the function we just made to all my data listed
+#clean up distubance data
+w_visits <- w_visits |>
+  rename(date = 1) |>
+  select(1:7) |>
+  unite(well, 5, 6, sep = "") |>
+  mutate(date = ymd_hms(paste(date, time))) |>
+  filter(date >= as.Date('2025-10-01 00:00:00')) |>
+  filter(observation == "removed")
 
-####Combine all dataframes so we can look at it ###
-do_data <- do_data %>% 
-  bind_rows (.id = "site") %>%
-  mutate(site = str_extract(site, "[^_]+$")) #take the well id out by the name
-
-ggplot (do_data, aes(date, do_mg_L, color = site)) + # view raw do by site
+ggplot (combined_do_data, aes(date, do_mg_L, color = site)) + # view raw do by site
   geom_line() +
-  facet_wrap(~site) +
+  facet_wrap(~well) +
+  geom_vline(data = w_visits, aes(xintercept = date, linetype = "dotted"))+
   theme_bw()+
   theme(legend.position = "none")
-
 
 #### clean up before pushing to github ####
 rm(list = ls()) #removing all things from the environment 
