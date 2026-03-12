33da30b9e84654c1c2ca180a8d82c41a1ba6c6d7
Jessie Ribera <jessierosaribera@gmail.com>
2026-03-12 16:50:44 -0600

    thinking about cleaning this script up a bit more

--- scripts/03_viewDOdata.R
+++ scripts/03_viewDOdata.R
@@ -72,7 +72,7 @@ ggplot (combined_do_data, aes(date, do_mg_L, color = site)) + # view raw do by s
 write_csv(combined_do_data,"~/Library/CloudStorage/OneDrive-UniversityofNewMexico/UNM/BEGI/Data/05_combined_cleaned/do_raw_combined.csv")
 
 
-#### generate and save plots to local drive ###
+#### generate and save plots to local drive ####
 for (i in seq_along(do_data)) {   # Make a for loop to make a plots for all data files
   p <- ggplot(do_data[[i]], aes(date, do)) + #plot by time and do
     geom_line() +
